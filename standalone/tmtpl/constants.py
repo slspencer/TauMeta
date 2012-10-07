4f5087de22b7f0efcb9af17e6c8fa878906adae9
susan <susan@susan-Vostro-3400.(none)>
2012-10-07 04:19:32 -0500

    Added pre-defined angles ANGLE90, ANGLE135, etc. to constants.py

--- standalone/tmtpl/constants.py
+++ standalone/tmtpl/constants.py
@@ -19,7 +19,23 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
+from math import pi
+
+def angleOfDegree(degree):
+    return degree * pi/180.0
+
+# angle constants
+ANGLE30 = angleOfDegree(30)
+ANGLE45 = angleOfDegree(45)
+ANGLE60 = angleOfDegree(60)
+ANGLE90 = angleOfDegree(90)
+ANGLE135 = angleOfDegree(135)
+ANGLE180 = angleOfDegree(180)
+ANGLE225 = angleOfDegree(225)
+ANGLE270 = angleOfDegree(270)
+ANGLE315 = angleOfDegree(315)
 # measurement constants
+
 IN_TO_CM = (2.54/1.0) #convert inches to centimeters
 CM_TO_IN = (1/2.54) #convert centimeters to inches
 
@@ -31,13 +47,15 @@ PT_TO_CM = (1/CM_TO_PT)
 IN_TO_PX = (90/1.0) #convert inches to pixels - Inkscape value
 CM_TO_PX = (90/2.54) #convert cm to px - Inkscape value
 MM_TO_PX=CM_TO_PX/10.0 # convert mm to px - Inkscape value
-CM=CM_TO_PX # shorthand for CM_TO_PX, useful in design file
-IN=IN_TO_PX # shorthand for IN_TO_PX, useful in design file
-MM=MM_TO_PX # need millimeters shorthand, too!
+CM = CM_TO_PX # shorthand for CM_TO_PX, useful in design file
+IN = IN_TO_PX # shorthand for IN_TO_PX, useful in design file
+MM = MM_TO_PX # need millimeters shorthand, too!
 
 PX_TO_IN = (1/IN_TO_PX) # convert pixels to inches - Inkscape value 90ppi
 PX_TO_CM = (1/CM_TO_PX) # convert pixels to centimeters - Inkscape
 PX_TO_MM = (1/MM_TO_PX) # convert pixels to millimeters - inkscape
+
+
 
 # sewing constants
 QUARTER_SEAM_ALLOWANCE = (IN_TO_PX*1/4.0) #1/4" seam allowance
