0d471bc03e0b92f4dd082d969142bdbc44b07c34
Susan Spencer <susan.spencer@gmail.com>
2014-07-09 17:48:05 -0500

    Added print option

--- standalone/tmtp.sh
+++ standalone/tmtp.sh
@@ -22,9 +22,10 @@
 # change the following two lines to match your installation
 cd /home/susan/src/tmtp-private/standalone
 
-TMTP_BASE=/home/susan/src/tmtp-private/standalone/
-PATTERN_BASE=$TMTP_BASE/patterns/
-CUSTOMER_BASE=$TMTP_BASE/customer/
+TMTP_BASE=/home/susan/src/tmtp-private/standalone
+PATTERN_BASE=$TMTP_BASE/patterns
+CUSTOMER_BASE=$TMTP_BASE/customer
+OUTPUT_BASE=$TMTP_BASE/output
 PYTHONPATH=$PYTHONPATH:$TMTP_BASE:$PATTERN_BASE:$CUSTOMER_BASE
 
 export PYTHONPATH
@@ -32,52 +33,102 @@ export TMTP_BASE
 export PATTERN_BASE
 export CUSTOMER_BASE
 
-function FileName () {
-    D=$(date +"%F"-%H%M)
-    FILE=${PATTERN##*/}
-    FILE=${FILE%%.*}
-    #FILE="$CUSTOMER_DIR/$FILE-$D"
-    echo $FILE
+function PrintPatternMenu () {
+    ans="$(zenity  --list  --text 'Print This Pattern?' --radiolist  --column '' --column 'Choice' TRUE 'Yes, Print this Pattern' FALSE 'No, do not Print this Pattern')"
+    
+    case $ans in
+        "Yes, Print this Pattern")
+          PRINTPATTERN="1";;
+        *)
+          PRINTPATTERN="0";;
+    esac
+    
+    echo PRINTPATTERN = $PRINTPATTERN
 
-    return;
-}
+    if [ PRINTPATTERN == '1' ]; then 
+        #run inkscape no gui (--z)  export to pdf (-A) 
+        inkscape --without-gui --file=$OUTPUT_BASE/$NAME/$OUTPUT_FILE.svg --export-area-snap \
+         -A $OUTPUT_BASE/$NAME/$OUTPUT_FILE.pdf \
+         | zenity --progress --title='Please wait, printing pattern...' \
+         --text='* Tip: Check ink prior to printing *' --auto-close
+        inkscape --without-gui --export-area-snap --export-pdf=/dev/stdout \
+         --file=$OUTPUT_BASE/$NAME/$OUTPUT_FILE.svg | lpr -P SC-T7000-Series
+        wait
+        wait
+        wait
+        wait
+        wait 
+        wait
+        wait 
+        echo "GOT TO HERE!"     
+    fi
+    
+    }
 
-function Tmtp () {
+function RunMkpattern () {
 
     #Include this option to show additional debug messages, and run ./tmtp.sh from a terminal:   --debug=prints $FILE.svg
 
     # run mkpattern script to generate the pattern
     #$TMTP_BASE/mkpattern --client=$CUSTOMER_NAME --pattern=$PATTERN --styles=$TMTP_BASE/tmtp_styles.json $FILE.svg
-    ./mkpattern --pattern=./patterns/$FILE.py --clientrecord=24 ./output/$FILE.svg
+    #./mkpattern --pattern=./patterns/$FILE.py --clientrecord=24 ./output/$FILE.svg
+    $TMTP_BASE/mkpattern --pattern=$PATTERN_BASE/$FILE.py --clientrecord=$RECORD_NO $OUTPUT_BASE/$NAME/$OUTPUT_FILE.svg
 
     #TODO: add if statement to run inkscape with reference layer visible or hidden.
 
     # run inkscape to outset the cutting lines and to view svg file.
-    inkscape --file=./output/$FILE.svg --verb=ZoomPage --select=A.cuttingline --select=B.cuttingline --select=C.cuttingline\
-    --select=D.cuttingline --select=E.cuttingline --select=F.cuttingline --select=G.cuttingline --select=H.cuttingline\
-    --select=I.cuttingline --select=J.cuttingline --select=K.cuttingline --select=S.cuttingline\
-    --verb=SelectionOffset --verb=EditDeselect --verb=FileSave | zenity --progress --title='Please wait, opening Inkscape...'\
+    inkscape --file=$OUTPUT_BASE/$NAME/$OUTPUT_FILE.svg --verb=ZoomPage \
+    --select=A.cuttingline --select=B.cuttingline --select=C.cuttingline \
+    --select=D.cuttingline --select=E.cuttingline --select=F.cuttingline \
+    --select=G.cuttingline --select=H.cuttingline --select=I.cuttingline \
+    --select=J.cuttingline --select=K.cuttingline --select=S.cuttingline \
+    --verb=SelectionOffset --verb=EditDeselect --verb=FileSave | zenity --progress \
+    --title='Please wait, opening Inkscape...'\
     --text='* Tip: Save pattern as PDF before printing *' --auto-close
-
-    # TODO: add menu item to print file - open inkscape without gui, save to pdf, use a linux print utility (not inkscape)
-    # inkscape --file=$FILE.svg --export-area-snap -A $FILE.pdf
+    
+    wait
+    wait
+    wait
+    PrintPatternMenu
 
     return;
     }
 
+function GetFileName () {
+    D=$(date +"%F"-%H%M)
+    FILE=${PATTERN##*/}
+    FILE=${FILE%%.*}
+    #FILE="$CUSTOMER_DIR/$FILE-$D"
+    echo $FILE
+    OUTPUT_FILE=$NAME-$FILE-$D
+    echo $OUTPUT_FILE
+
+    return;
+}
+
 function CustomerMenu () {
-
     # Display menu and interact based on the user's input
-
     CUSTOMER_NAME="$(zenity  --file-selection\
  --title '*        Select A Customer:                     *'\
  --filename=$CUSTOMER_BASE\
  --file-filter='*.json')"
-
     CUSTOMER_DIR=${CUSTOMER_NAME%/*}
-
     return;
     }
+
+function RecordMenu () {
+    ans="$(zenity  --list  --text 'Select Measurements' --radiolist  --column '' --column 'Measurements' TRUE 'DressFormLg' FALSE 'Susan')"
+    case $ans in
+        "DressFormLg")
+          NAME="DressFormLg";
+          RECORD_NO="27";;
+        "Susan")
+          NAME="Susan";
+          RECORD_NO="24";;
+    esac
+    return;
+    }
+
 
 function PatternMenu () {
 
@@ -85,52 +136,47 @@ function PatternMenu () {
     #Display menu and interact based on the user's input
     PATTERN="$(zenity  --file-selection\
  --title '*        Select A Pattern:                       *'\
- --filename=$PATTERN_BASE --file-filter='*.py')"
+ --filename=$PATTERN_BASE/ --file-filter='*.py')"
     echo $PATTERN
     return;
     }
 
-function MainMenu () {
-    # Loop until user selects EXIT
-    # TODO: add option to open existing pattern .svg file\
-    # TODO: make this script more robust
-    # TODO: either remove the Cancel & OK buttons or make them work
-
+function CreateMenu () {
     var1="$(zenity --list --radiolist\
  --text '*        Welcome to Tau Meta Tau Physica                               *'\
  --column='' --column='What do you want to do?'\
  FALSE 'Create a Pattern with Reference Points'\
  FALSE 'Exit' )"
-
     case $var1 in
         "Create a Pattern with Reference Points")
-          GETPATTERN="1";;
+          CREATEPATTERN="1";;
         *)
-          GETPATTERN="0";;
+          CREATEPATTERN="0";;
     esac
-
     return;
     }
 
-# Main Program
+#-------------
+# Main
 
 GETPATTERN="1"
 
 while true; do
 
     PATTERN=""
+    RECORD_NO=""
     CUSTOMER_NAME=""
     CUSTOMER_DIR=""
 
-    MainMenu
-
-    if [ $GETPATTERN == '0' ]; then
-        break
-    else
+    CreateMenu
+    if [ $CREATEPATTERN == '1' ]; then
         PatternMenu
         #CustomerMenu
-        FileName
-        Tmtp
+        RecordMenu
+        GetFileName
+        RunMkpattern
+    else
+        break
     fi
 
 done
